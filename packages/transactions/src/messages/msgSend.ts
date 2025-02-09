a5e0f06de86c9d687fc560a5cc0891d6e66b676d
jiujiteiro <romeiroe@gmail.com>
2025-02-09 17:26:03 -0500

    feat: allow bank send to build body separately

--- packages/transactions/src/messages/msgSend.ts
+++ packages/transactions/src/messages/msgSend.ts
@@ -1,6 +1,8 @@
 import {
   createMsgSend as protoMsgSend,
   createTransaction,
+  createBodyWithMultipleMessages,
+  createTransactionWithBody,
 } from '@realiotech/proto'
 
 import { Chain, Fee, Sender } from './common'
@@ -28,6 +30,47 @@ export function createTxMessageSend(
   const tx = createTransaction(
     msgSend,
     memo,
+    fee.amount,
+    fee.denom,
+    parseInt(fee.gas, 10),
+    sender.pubkey,
+    sender.sequence,
+    sender.accountNumber,
+    chain.cosmosChainId,
+  )
+
+  return {
+    signDirect: tx.signDirect,
+    legacyAmino: tx.legacyAmino,
+  }
+}
+
+export function createMsgSendBody(
+  sender: Sender,
+  memo: string,
+  params: MessageSendParams,
+) {
+  // Cosmos
+  const msgSend = protoMsgSend(
+    sender.accountAddress,
+    params.destinationAddress,
+    params.amount,
+    params.denom,
+  )
+  const body = createBodyWithMultipleMessages([msgSend], memo)
+
+  return { body }
+}
+
+export function createSendTx(
+  body: any,
+  chain: Chain,
+  sender: Sender,
+  fee: Fee,
+) {
+  // Cosmos
+  const tx = createTransactionWithBody(
+    body,
     fee.amount,
     fee.denom,
     parseInt(fee.gas, 10),
